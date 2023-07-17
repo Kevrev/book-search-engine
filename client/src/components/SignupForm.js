c5bc3f9c19a6eac24a9b5d956fbf7de72b4d3e98
Seer <ksmnk@protonmail.com>
2023-07-17 00:27:51 -0700

    added mutation to signupform.js

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -3,6 +3,8 @@ import { Form, Button, Alert } from 'react-bootstrap';
 
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations'
 
 const SignupForm = () => {
   // set initial form state
@@ -28,13 +30,15 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const { data } = await ADD_USER({
+        variables: { username, email, password }
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
 
-      const { token, user } = await response.json();
+      const { token, user } = data.ADD_USER;
       console.log(user);
       Auth.login(token);
     } catch (err) {
