4351256eea16004852be2b4be2393a7bd280c387
Seer <ksmnk@protonmail.com>
2023-07-16 20:24:39 -0700

    working on auth

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const { authMiddleware } = require('./utils/auth');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
